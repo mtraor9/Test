20c9e946e1de5a56b6fc41a279a736f328f7ce3f
mtraore9 <moryfodetraore96@outlook.com>
2020-05-07 10:58:30 -0400

    f

---
+++ t.py
@@ -0,0 +1,7 @@
+name = input("Enter your name:")
+print("Hello, " +name)
+age = input("Please enter your age:")
+if age > 3 or 8:
+    print("You are allowed to use the internet.")
+elif age <= 3:
+    print("You are still a kid what are you doing here.")
---
+++ tr.py
@@ -0,0 +1,12 @@
+a=input('Enter:')
+b=input('Enter:')
+c = a + b
+d=[]
+e=6
+if c < e:
+    print("  *****        Baby Kadi  T'es my Number One        ***** \n")
+    print("******** Je suis Amoureux de ton Ethic et Esthetic ********")
+else:
+    d.append(c)
+    d < 9
+    print(d)
